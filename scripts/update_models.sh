f94c393d779c27c1db79e8a93835a544000300e5
Keith Bloemer <32459398+GuitarML@users.noreply.github.com>
2021-06-04 13:28:44 -0500

    Create update_models.sh

---
+++ scripts/update_models.sh
@@ -0,0 +1,33 @@
+#############################################################################
+# NeuralPi - Update Models - Mac/Linux Script
+#
+# This script transfers models from a Linux or Mac computer to the NeuralPi,
+#     and from the NeuralPi back to the host computer. Edit the Raspberry Pi
+#     IP address (after connecting to a local Wifi Network), and run this
+#     script from a Linux/Mac computer running the NeuralPi plugin.
+#
+# Note: Ensure that all models have unique names or they will be overwritten
+# Note: If prompted when connecting, type "yes" and hit enter
+#############################################################################
+
+# USER INPUTS #
+
+rpi_ip_address=127.0.0.1  # Update this field with the Raspberry Pi's IP address
+
+
+# Uncomment the appropriate path for your computer:
+
+# host_model_path=~/.config/GuitarML/NeuralPi/tones   #Typical Linux Path
+# host_model_path=/Library/GuitarML/NeuralPi/tones    #Typical Mac Path (System Install)
+# host_model_path=~/Library/GuitarML/NeuralPi/tones   #Typical Mac Path (User Install)
+
+
+rpi_model_path=/home/mind/.config/GuitarML/NeuralPi/tones # Rpi with Elk OS Path (shouldn't need to change)
+
+#############################################################################
+
+# Copy all models from local computer to Rpi
+scp $host_model_path/*.json  root@$rpi_ip_address:$rpi_model_path/
+
+# Copy all models from Rpi to local computer
+scp root@$rpi_ip_address:$rpi_model_path/*.json  $host_model_path/
